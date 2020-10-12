59b25254912ad076b1c28e11419c6e9d99d1e8e3
Nechelley <MrNechelley@gmail.com>
2020-10-12 01:11:12 -0300

    Initial things from Spring Initializr

---
+++ src/main/java/br/com/study/theKnowledgeDungeon/TheKnowledgeDungeonApplication.java
@@ -0,0 +1,13 @@
+package br.com.study.theKnowledgeDungeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheKnowledgeDungeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheKnowledgeDungeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/study/theKnowledgeDungeon/TheKnowledgeDungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.theKnowledgeDungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheKnowledgeDungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
